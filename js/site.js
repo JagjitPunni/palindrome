38532842218057e0d5c41e6714eb2adf70ddc495
Jagjit Punni <j.punni@gmx.de>
2023-02-21 02:57:44 +0100

    Update site.js
    Added functions to check for palindrome, reverse string, and display result to user

--- js/site.js
+++ js/site.js
@@ -1,4 +1,66 @@
-//Call Hello World
-function helloWorld(){
-    alert('Hello World');
+//Get the values (string) from the page
+//Controller function
+function getValue(){
+
+    //make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+    //get user string from the page
+    let userString = document.getElementById('userString').value;
+
+    //check for a palindrome
+    let returnObj = palindrom(userString);
+
+    //display out string to the screen
+    displayString(returnObj);
+
+}
+
+
+
+
+
+//Reverse the string
+//Logic function
+function palindrom(userString){
+
+    //level
+    userString = userString.toLowerCase();
+
+    //remove special character and spaces
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+
+    let revArray = [];
+    let returnObj = {};
+     //reverse a string using a for loop
+
+    for (let i = userString.length -1 ; i >= 0; i--) {
+        revArray += userString[i];
+        
+    }
+
+    if (revArray == userString) {
+        returnObj.msg = "Well Done! You entered a palindrome!";
+    }else{
+        returnObj.msg = "Sorry! You did not entered a palindrome!"
+    }
+    
+    returnObj.reversed = revArray;
+
+    return returnObj;
+
+}
+
+
+
+
+
+//Display the reversed string to the user
+//View function
+function displayString(returnObj){
+
+    document.getElementById("alertHeader").innerHTML =  returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
